1c42b0fbff036c9930ce3d10c39a971e5aa3e91c
slava136 <94437092+slava136@users.noreply.github.com>
2021-11-30 10:38:17 +0300

    Add files via upload

---
+++ Задача 14/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int z1 = 0;
+            if (radioButton1.Checked == true)
+            { z1 = 750; }
+
+            string hub = textBox1.Text;
+            int hub1 = Convert.ToInt32(hub);
+            int vivod = hub1 * 50 + z1;
+
+            label3.Text = Convert.ToString(vivod);
+        }
+    }
+}
